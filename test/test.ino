e7dae5f906ef4c340a38e122b5081d114a39e813
Vinicius Couto Espindola <vinicius.c.e@hotmail.com>
2019-08-29 17:51:36 -0300

    Changed hash to path. Not random, but a path is create for every data (not good).

--- test/test.ino
+++ test/test.ino
@@ -4,6 +4,8 @@
 //    ArduinoJson (5.13.1) - by Benoit Blanchon
 //    FirebaseExtended - https://github.com/FirebaseExtended/firebase-arduino/releases
 
+#include <stdio.h>
+
 #include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
 #include <FirebaseArduino.h>
@@ -19,18 +21,22 @@ DHT dht(DHTPIN, DHTTYPE);
 // Set these to run example.
 #define FIREBASE_HOST "teste-bb0d8.firebaseio.com"
 #define FIREBASE_AUTH "mLOkguUxVGWlbYmAWdUYiHaYqWqDF9wHstkUIfTT"
-#define WIFI_SSID "Shimbalaie_2.4GHz"
-#define WIFI_PASSWORD "pipolina"
+#define WIFI_SSID "iPhone de vinicius"
+#define WIFI_PASSWORD "shimbalaie"
 
 String myString;
 int vr = A0; // variable resistor connected 
 int sdata = 0; // The variable resistor value will be stored in sdata.
+StaticJsonBuffer<200> jb;
 
 // Function which reads and sends the data
 void sendSensor()
 {
+  static int count = 0;
   float h = dht.readHumidity();
   float t = dht.readTemperature();
+  char path[20];
+  char value[20];
   
   // Discard flawed reads
   if (isnan(h) || isnan(t)) {
@@ -38,8 +44,14 @@ void sendSensor()
     return;
   }
 
-  Firebase.pushFloat("hum", h);
-  Firebase.pushFloat("temp", t);
+  sprintf(path, "hum\\%d", count);
+//  sprintf(buff, "{\"%d\":\"%f\"}", count, h);
+//  JsonVariant variant = jb.parse(buff);
+
+  Firebase.setFloat(path,h);
+//  Firebase.setFloat("hum", h);
+//  Firebase.setFloat("temp", t);
+  ++count;
 }
 
 void setup()
