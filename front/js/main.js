16cd03c776997b77ab58c7e0fad64567be93205e
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-09-12 14:09:56 -0300

    Convert .json data to .csv

--- front/js/main.js
+++ front/js/main.js
@@ -1,6 +1,10 @@
 'use strict';
 
-// Your web app's Firebase configuration
+// call init() once the whole page has loaded
+window.addEventListener("load", init, false);
+
+///////////////////////////////////////////////////////////////////////////////
+
 const firebaseConfig = {
     apiKey: "AIzaSyATSHBpjnyHaTb8bF-NmWrzam4KZ5Amrh0",
     authDomain: "teste-bb0d8.firebaseapp.com",
@@ -11,40 +15,7 @@ const firebaseConfig = {
     appId: "1:701434842650:web:fdf7e8b3d377c1b9"
 };
 
-window.addEventListener('load', init, false);
-
-function update_text_with_ref(text, ref, index, strs) {
-    // FIXME improve 'index' and 'strs' solution
-    ref
-        .once("value")
-        .then(function (snapshot) {
-            let values = snapshot.val(); // "last"
-            const keys = Object.keys(values); // FIXME temporary workaround for current firebase data format
-            console.log(`${strs[0]} values: ${values}`);
-            const key = keys[0];
-
-            const value_keys = Object.keys(values[key]);
-            const i = index % value_keys.length
-            const value = values[keys[0]][value_keys[i]];
-            text.innerHTML = `${value} ${strs[1]} (${key}:${value_keys[i]})`;
-
-        });
-}
-
 function init() {
-    // Initialize Firebase
+    console.log("Initializing Firebase...");
     firebase.initializeApp(firebaseConfig);
-
-    const humidity_text = document.getElementById("humidade");
-    const temperature_text = document.getElementById("temperatura");
-
-    const humidity_ref = firebase.database().ref("hum");
-    const temperature_ref = firebase.database().ref("tmp");
-
-    let index = 0;
-    // setInterval(() => {
-    //     update_text_with_ref(humidity_text, humidity_ref, index, ["hum", "RH"]);
-    //     update_text_with_ref(temperature_text, temperature_ref, index, ["tmp", "°C"]);
-    //     index++;
-    // }, 1000);
 }
