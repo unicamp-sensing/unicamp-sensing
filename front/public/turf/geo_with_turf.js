ab96e5de0931714ce8b0f39ec909df1395dfc868
Tiago Loureiro Chaves <ra187690@students.ic.unicamp.br>
2019-10-15 17:16:32 -0300

    Add data layers on top of the map

--- front/public/turf/geo_with_turf.js
+++ front/public/turf/geo_with_turf.js
@@ -1,19 +1,47 @@
 const map = L.map("mapid").setView([-22.814, -47.069], 16);
-initMap();
+map.scrollWheelZoom.disable();
 
-function initMap() {
-    // get the stamen toner-lite tiles
-    const tiles = L.tileLayer(
-        "http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}", {
-            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> — Map data © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            subdomains: "abcd",
-            minZoom: 0,
-            maxZoom: 20,
-            ext: "png"
-        });
+const tileUrlsOSMDE = "http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png";
+const tilesOSMDE = L.tileLayer(tileUrlsOSMDE, {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
 
-    tiles.addTo(map);
+const tileUrlSTL = "http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}";
+const tilesSTL = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+});
 
-    //disable scroll wheel zoom
-    map.scrollWheelZoom.disable();
+// layers
+const red    = new L.layerGroup();
+const blue   = new L.layerGroup();
+const green  = new L.layerGroup();
+const purple = new L.layerGroup();
+
+addData();
+
+const toggleMaps = {
+    "OSM DE": tilesOSMDE,
+    "Stamen Toner Lite": tilesSTL
+};
+const overlayMaps = {
+    "red": red,
+    "blue": blue,
+    "green": green,
+    "purple": purple
+};
+L.control.layers(toggleMaps, overlayMaps).addTo(map);
+
+
+function addData() {
+    // mock some data
+    // FIXME TODO add real values from Firebase
+    L.circle([37.795, -122.26], 500, {color:"red"}).addTo(red);
+    L.circle([37.790, -122.26], 500, {color:"blue"}).addTo(blue);
+    L.circle([37.785, -122.26], 500, {color:"green"}).addTo(green);
+    L.circle([37.780, -122.26], 500, {color:"purple"}).addTo(purple);
 }
