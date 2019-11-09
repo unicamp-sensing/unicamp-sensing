e04773422cd60775a576a84a714c594b8448f2f3
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-11-09 17:54:23 -0300

    Make check generic to new values addition

--- front/public/constants.js
+++ front/public/constants.js
@@ -1,20 +1,18 @@
 const valueKeys = ["tmp", "hum", "pm10", "pm25"];
 
-
 const valueKeyToRange = {
-    tmp:  {min: 0, max: 40}, // temperature
-    hum:  {min: 0, max: 150}, // humidity
-    pm10: {min: 0, max: 200}, // PM 10
-    pm25: {min: 0, max: 250}  // PM 2.5
+    tmp:  { min: 0, max: 40 },  // temperature
+    hum:  { min: 0, max: 150 }, // humidity
+    pm10: { min: 0, max: 200 }, // PM 10
+    pm25: { min: 0, max: 250 }  // PM 2.5
 };
 
 const valueToColor = {
     tmp:  { scale: "Bluered", reverse: false },
-    hum:  { scale: "YlGnBu",  reverse: true },
-    pm10: { scale: "YlOrRd",  reverse: true },
-    pm25: { scale: "Hot",     reverse: true },
+    hum:  { scale: "YlGnBu",  reverse: true  },
+    pm10: { scale: "YlOrRd",  reverse: true  },
+    pm25: { scale: "Hot",     reverse: true  },
 };
-
 
 const valueKeyToLayer = { tmp: "Temperature", hum: "Humidity", pm10: "PM10", pm25: "PM2.5" };
 
--- front/public/parse.js
+++ front/public/parse.js
@@ -1,4 +1,3 @@
-
 async function getRawData() {
     const dataRef = firebase.database().ref("/");
     const dataSnapshot = await dataRef.once("value");
@@ -9,16 +8,16 @@ async function getRawData() {
 function boardValuesByDayDate(data) {
     const valuesByDayDate = {};
     for (const year in data) {
-      if (parseInt(year) < 2019){ continue;}
-          for (const month in data[year]) {
+        if (parseInt(year) < 2019) { continue; }
+        for (const month in data[year]) {
             for (const day in data[year][month]) {
                 const values = [];
                 for (const hour in data[year][month][day]) {
                     for (const min in data[year][month][day][hour]) {
                         for (const sec in data[year][month][day][hour][min]) {
                             for (const board in data[year][month][day][hour][min][sec]) {
-                                  const date = new Date(year, month, day, hour, min, sec);
-                                  if(sensorRangeValid(data[year][month][day][hour][min][sec][board])){
+                                const date = new Date(year, month, day, hour, min, sec);
+                                if (sensorRangeValid(data[year][month][day][hour][min][sec][board])) {
                                     values.push({
                                         x: date,
                                         y: data[year][month][day][hour][min][sec][board]
@@ -35,14 +34,7 @@ function boardValuesByDayDate(data) {
     return valuesByDayDate;
 }
 
-function in_range(values, key) {
-    return (values[key] < valueKeyToRange[key].min || values[key] > valueKeyToRange[key].max)
-}
-
 function sensorRangeValid(values) {
-    if (in_range(values, 'tmp')) return false;
-    if (in_range(values, 'hum')) return false;
-    if (in_range(values, 'pm10')) return false;
-    if (in_range(values, 'pm25')) return false;
-    return true;
+    const inRange = (key) => valueKeyToRange[key].min <= values[key] && values[key] <= valueKeyToRange[key].max;
+    return valueKeys.map(key => inRange(key)).every(x => x); // true iff all values are in the valid range
 }
