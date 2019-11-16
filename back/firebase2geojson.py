9448550f11633ad04bb2a6e5eebdbe086c55ea7e
Tiago Chaves <tiagoloureirochaves@gmail.com>
2019-11-16 10:16:17 -0300

    Convert data from 19-11-16 to GeoJSON format

--- back/firebase2geojson.py
+++ back/firebase2geojson.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import geojson
 from geojson import Feature, Point, FeatureCollection
 
 def board_values(required_keys=['lat', 'lon'], print_errors=False):
@@ -16,6 +17,7 @@ def board_values(required_keys=['lat', 'lon'], print_errors=False):
                             for second, _second in _minute.items():
                                 for board_MAC_addr, _board_values in _second.items():
                                     if all(key in _board_values.keys() for key in required_keys):
+                                        _board_values['board_MAC_addr'] = board_MAC_addr
                                         triplets.append((_board_values, f"{year}-{month}-{day}", f"{hour}:{minute}:{second}"))
                     except Exception as e:
                         if print_errors:
@@ -34,22 +36,25 @@ def triplet2feature(triplet, kept_keys=['hum', 'tmp', 'pm10', 'pm25', 'lat', 'lo
     return Feature(geometry=Point((lat, lon)), properties=properties)
 
 def convert(firebase_json):
-    ''' Converts `firebase_json` from a Firebase structured string to the GeoJSON format '''
+    ''' Converts `firebase_json` from a Firebase structured string to a `geojson.FeatureCollection` '''
     feature_list = [triplet2feature(triplet) for triplet in board_values()]
-    feature_collection = FeatureCollection(feature_list)
-    geo_json = str(feature_collection)
-    return geo_json
+    return FeatureCollection(feature_list)
 
 if __name__ == "__main__":
     try:
         path = sys.argv[1]
+        out_path = sys.argv[2]
+        indent = None if len(sys.argv) <= 3 else int(sys.argv[3]) # leave as None for a 'minified' output
     except:
-        print("usage: python firebase2geojson.py teste-bb0d8-export.json > out.json")
+        print("usage: python firebase2geojson.py teste-bb0d8-export.json out.json [indent]")
         exit()
     
-    with open(path) as json_file:
+    with open(path, 'r') as json_file:
         firebase_json = json_file.read()
         firebase_json = json.loads(firebase_json)
     
-    print(convert(firebase_json))
+    with open(out_path, 'w+') as geojson_file:
+        geojson.dump(convert(firebase_json), geojson_file, indent=indent)
+    
+    print("done.")
     
