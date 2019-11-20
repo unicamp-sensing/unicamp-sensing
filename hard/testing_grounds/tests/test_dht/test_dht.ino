abd623b6b4c81eeffa215db47993626ea45e1b92
vinicius espindola <vinicius.c.e@hotmail.com>
2019-11-20 08:05:18 -0300

    Adapted dht test file

--- hard/testing_grounds/tests/test_dht/test_dht.ino
+++ hard/testing_grounds/tests/test_dht/test_dht.ino
@@ -8,19 +8,25 @@ void setup() {
   Serial.begin(115200);
   // put your setup code here, to run once:
   dht.begin();
+  delay(3000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   dht.update();
-  Serial.print("HUM: ");
-  Serial.println(dht.hum);
-  Serial.print("TMP: ");
-  Serial.println(dht.tmp);
 
-  bool test = !isnan(dht.tmp) && !isnan(dht.hum);
-  if (test) Serial.println("Pass");
-  else Serial.println("Fail");
+  for (int i = 0; i < 5; ++i) {
+    Serial.print("HUM: ");
+    Serial.println(dht.hum);
+    Serial.print("TMP: ");
+    Serial.println(dht.tmp);
+    if (isnan(dht.tmp) || isnan(dht.hum)) {
+      Serial.println("Fail");
+    }
+    delay(1000);
+  }
+  
+  Serial.println("Pass");
 
   delay(1000);
 }
